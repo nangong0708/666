7f96c48a250e62c896036cee2dffcb076d0232bd
nangong0708 <302806838@qq.com>
2018-12-18 12:51:33 +0800

    Add files via upload

---
+++ 22.js
@@ -0,0 +1,32 @@
+function initEcAd() {
+document.all.AdLayer1.style.posTop = -200;
+document.all.AdLayer1.style.visibility = 'visible'
+document.all.AdLayer2.style.posTop = -200;
+document.all.AdLayer2.style.visibility = 'visible'
+MoveLeftLayer('AdLayer1');
+MoveRightLayer('AdLayer2');
+}
+function MoveLeftLayer(layerName) {
+var x = 5;
+var y = 100;// 左侧广告距离页首高度
+var diff = (document.body.scrollTop + y - document.all.AdLayer1.style.posTop)*.40;
+var y = document.body.scrollTop + y - diff;
+eval("document.all." + layerName + ".style.posTop = parseInt(y)");
+eval("document.all." + layerName + ".style.posLeft = x");
+setTimeout("MoveLeftLayer('AdLayer1');", 20);
+}
+function MoveRightLayer(layerName) {
+var x = 5;
+var y = 100;// 右侧广告距离页首高度
+var diff = (document.body.scrollTop + y - document.all.AdLayer2.style.posTop)*.40;
+var y = document.body.scrollTop + y - diff;
+eval("document.all." + layerName + ".style.posTop = y");
+eval("document.all." + layerName + ".style.posRight = x");
+setTimeout("MoveRightLayer('AdLayer2');", 20);
+}
+
+document.write("<div id=AdLayer1 style='position: fixed;top: 50%;left: 0;  margin-top: -100px;visibility:hidden;z-index:1'><a href='http://www.163.com' target='_blank'><img src=http://121.12.92.31:8080/images/2.gif border='0'></a></div>"
++"<div id=AdLayer2 style='position: fixed;top: 50%; right: 0; margin-top: -100px;visibility:hidden;z-index:1'><a href='http://www.163.com' target='_blank'><img src=http://121.12.92.31:8080/images/2.gif border='0'></a></div>");
+initEcAd()
+
+// JavaScript Document
